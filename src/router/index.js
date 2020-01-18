50b7c92acc1bf8cce6754ec9185eb4e4f03b77d6
luk3my <luk3myoung@gmail.com>
2020-01-18 11:42:52 +1000

    check heroku url

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,62 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home.vue";
+import AddPost from "../components/Posts/AddPost.vue";
+import Posts from "../components/Posts/Posts.vue";
+import Post from "../components/Posts/Post.vue";
+
+import Profile from "../components/Auth/Profile.vue";
+import Signin from "../components/Auth/Signin.vue";
+import Signup from "../components/Auth/Signup.vue";
+
+import AuthGaurd from '../AuthGaurd'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
+{
+    path: "/",
+    name: "home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/posts",
+    name: "Posts",
+    component: Posts
+  },
+   {
+    path: "/posts/:postId",
+    name: "Post",
+    component: Post,
+    props: true
+  },
+  {
+    path: "/post/add",
+    name: "AddPost",
+    component: AddPost,
+    beforeEnter: AuthGaurd
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+    beforeEnter: AuthGaurd
+  },
+  {
+    path: "/signin",
+    name: "Signin",
+    component: Signin
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
